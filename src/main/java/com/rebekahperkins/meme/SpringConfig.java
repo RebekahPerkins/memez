9cdab95659ce669c3a15de7f0dc46392f20a8761
RebekahPerkins <r.perkins.smith@gmail.com>
2017-06-01 18:25:56 -0700

    Spring config, main method, working server, gitignore weirdness

---
+++ src/main/java/com/rebekahperkins/meme/SpringConfig.java
@@ -0,0 +1,11 @@
+package com.rebekahperkins.meme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class SpringConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConfig.class, args);
+    }
+}
